2788f731850d9f2feaf7601c08cb1973c2c90405
Leonardo Scalabrini <leonardo_scalabrini@hotmail.com>
2022-10-16 16:47:45 -0300

    Movido para pasta correta

--- src/test/java/strategys/impl/MatchTest.java
+++ src/test/java/strategys/impl/MatchTest.java
@@ -1,22 +1,19 @@
-package iterators.impl;
+package strategys.impl;
 
 import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import domains.Player;
+import fixtures.SingleMatchStrategyFixture;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
-
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.NullSource;
-
-import domains.Player;
-import fixtures.SingleMatchStrategyFixture;
-import strategys.impl.SingleMatchStrategy;
 
 @Timeout(value = 100, unit = TimeUnit.MILLISECONDS)
 class MatchTest {
