5b738e58193ef6144084996f7469a34ea1e10e77
Leonardo Scalabrini <leonardo_scalabrini@hotmail.com>
2022-10-17 17:32:41 -0300

    Format code

--- src/main/java/domains/Tournament.java
+++ src/main/java/domains/Tournament.java
@@ -1,9 +1,8 @@
 package domains;
 
-import io.reactivex.Observable;
-
 import static java.util.Objects.requireNonNull;
 
+import io.reactivex.Observable;
 import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -22,31 +21,35 @@ public class Tournament {
   public Optional<Player> tournamentWinner() {
     final AtomicInteger size = new AtomicInteger(players.size());
     final AtomicInteger currentIndex = new AtomicInteger(-1);
-    Observable<int[]> stream = Observable.create(emitter -> {
-      while (size.get() > 1) {
-        if (currentIndex.get() >= size.get() - 2) {
-          currentIndex.set(-1);
-        }
-        emitter.onNext(new int[]{currentIndex.incrementAndGet(), currentIndex.incrementAndGet()});
-      }
-      emitter.onComplete();
-    });
+    Observable<int[]> stream =
+        Observable.create(
+            emitter -> {
+              while (size.get() > 1) {
+                if (currentIndex.get() >= size.get() - 2) {
+                  currentIndex.set(-1);
+                }
+                emitter.onNext(
+                    new int[] {currentIndex.incrementAndGet(), currentIndex.incrementAndGet()});
+              }
+              emitter.onComplete();
+            });
 
     stream
-    .doOnNext(indexList -> {
-      int indexOne = indexList[0];
-      int indexTwo = indexList[1];
-      Player playerOne = requireNonNull(players.get(indexOne));
-      Player playerTwo = requireNonNull(players.get(indexTwo));
-      int indexToRemove = indexOne;
-      if (playerOne.strategy.beats(playerTwo.strategy)) {
-        indexToRemove = indexTwo;
-      }
-      players.remove(indexToRemove);
-      size.decrementAndGet();
-      currentIndex.decrementAndGet();
-    })
-    .publish();
+        .doOnNext(
+            indexList -> {
+              int indexOne = indexList[0];
+              int indexTwo = indexList[1];
+              Player playerOne = requireNonNull(players.get(indexOne));
+              Player playerTwo = requireNonNull(players.get(indexTwo));
+              int indexToRemove = indexOne;
+              if (playerOne.strategy.beats(playerTwo.strategy)) {
+                indexToRemove = indexTwo;
+              }
+              players.remove(indexToRemove);
+              size.decrementAndGet();
+              currentIndex.decrementAndGet();
+            })
+        .publish();
     return players.stream().findFirst();
   }
 }
